8cfbb5c052d4f8b0c1023b63fb7054908f66eef1
Maarten van Grootel <maarten.vangrootel@shopify.com>
2017-10-06 09:36:02 -0400

    Completely purge UPS integration from library as requested by UPS

---
+++ test/remote/kunaki_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class RemoteKunakiTest < ActiveSupport::TestCase
+  include ActiveShipping::Test::Fixtures
+
+  def setup
+    @carrier = Kunaki.new
+    @item1 = { :sku => 'XZZ1111111', :quantity => 2 }
+    @item2 = { :sku => 'PXZZ111112', :quantity => 1 }
+    @items = [@item1, @item2]
+  end
+
+  def test_successful_rates_request
+    response = @carrier.find_rates(
+                 location_fixtures[:ottawa],
+                 location_fixtures[:beverly_hills],
+                 package_fixtures.values_at(:book, :wii),
+                 :items => @items
+               )
+
+    assert response.success?
+    assert_equal 4, response.rates.size
+    assert_equal ["UPS 2nd Day Air", "UPS Ground", "UPS Next Day Air Saver", "USPS First Class Mail"], response.rates.collect(&:service_name).sort
+  end
+
+  def test_send_no_items
+    assert_raises(ActiveUtils::ResponseError) do
+      @carrier.find_rates(
+        location_fixtures[:ottawa],
+        location_fixtures[:beverly_hills],
+        package_fixtures.values_at(:book, :wii),
+        :items => []
+      )
+    end
+  end
+end
